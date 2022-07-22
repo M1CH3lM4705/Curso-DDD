ff7c4628ad56c56de1563b9902c4b3d57a514641
Michel <michel.mef@gmail.com>
2022-07-22 13:23:45 -0400

    Facace comentado

--- parte2/Facedes/EmpresaFacade.cs
+++ parte2/Facedes/EmpresaFacade.cs
@@ -7,28 +7,28 @@ namespace Curso_DDD.parte2.Facedes
 {
     public class EmpresaFacade
     {
-        public Cliente BuscaCliente(string cpf)
-        {
-            return new ClienteDao().BuscarPorCpf(cpf);
-        }
+        // public Cliente BuscaCliente(string cpf)
+        // {
+        //     return new ClienteDao().BuscarPorCpf(cpf);
+        // }
 
-        public Fatura CriaFatura(Cliente cliente, double valor)
-        {
-            return new Fatura(cliente, valor);
-        }
+        // public Fatura CriaFatura(Cliente cliente, double valor)
+        // {
+        //     return new Fatura(cliente, valor);
+        // }
 
-        public Cobranca GeraCobranca(Tipo tipo, Fatura fatura)
-        {
-            Cobranca cobranca = new Cobranca(tipo, fatura);
-            cobranca.Emite();
-            return cobranca;
-        }
+        // public Cobranca GeraCobranca(Tipo tipo, Fatura fatura)
+        // {
+        //     Cobranca cobranca = new Cobranca(tipo, fatura);
+        //     cobranca.Emite();
+        //     return cobranca;
+        // }
 
-        public ContatoCliente FazContato(Cliente cliente, Cobranca cobranca)
-        {
-            ContatoCliente contato = new ContatoCliente(cliente, cobranca);
-            contato.Dispara();
-            return contato;
-        }
+        // public ContatoCliente FazContato(Cliente cliente, Cobranca cobranca)
+        // {
+        //     ContatoCliente contato = new ContatoCliente(cliente, cobranca);
+        //     contato.Dispara();
+        //     return contato;
+        // }
     }
 }
